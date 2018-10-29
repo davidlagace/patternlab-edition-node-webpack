cfe1b9ec14542ee9307c1205eb40e5369efc6bc3
dlagace91 <H0rusR@Mu>
2018-10-29 16:51:37 -0400

    Add working prod compile version of patternlab project

--- source/_app/webpack.app.js
+++ source/_app/webpack.app.js
@@ -1,10 +1,102 @@
-const {getIfUtils} = require('webpack-config-utils');
+const webpack = require("webpack");
+const {getIfUtils} = require("webpack-config-utils");
+const {resolve} = require("path");
+const globby = require("globby");
+const plConfig = require("../../patternlab-config.json");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = env => {
-    const {ifProd, ifDev} = getIfUtils(env);
+  const {ifProduction, ifDevelopment} = getIfUtils(env);
+  const appNamespace = plConfig.app.namespace
+                       ? `$ns:${plConfig.app.namespace};`
+                       : ``;
+  const app = {
+    entry: {
+      "css/uikit-marvel": globby.sync([
+        resolve(`${plConfig.paths.source.css}uikit-marvel.scss`)
+      ]).map(function(filePath) {
+        return filePath;
+      }),
 
-    const app = {
-        //Custom webpack configuration goes here
+      // "js/uikit-marvel": globby.sync([
+      //     resolve(`${plConfig.paths.source.patterns}**/*.js`),
+      //     "!**/*.test.js"
+      //   ], {gitignore: true}
+      // )
+    },
+    optimization: {
+      splitChunks: {
+        cacheGroups: {
+          vendor: {
+            test: /node_modules/,
+            chunks: "initial",
+            name: "js/uikit-marvel-vendor",
+            priority: 10,
+            enforce: true
+          }
+        }
+      }
+    },
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: "[name].css",
+        disable: ifDevelopment()
+      }),
+      new webpack.DefinePlugin({
+        NAMESPACE: appNamespace
+      })
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.scss$/,
+          use: [
+            {
+              loader: ifDevelopment(
+                "style-loader",
+                MiniCssExtractPlugin.loader
+              )
+            },
+            {
+              loader: "css-loader",
+              options: {
+                url: false,
+                minimize: {
+                  safe: ifProduction()
+                },
+                sourceMap: ifDevelopment(),
+              }
+            },
+            {
+              loader: "postcss-loader",
+              options: {
+                sourceMap: ifDevelopment(),
+                plugins: loader => [
+                  // eslint-disable-line no-unused-vars
+                  require("autoprefixer"),
+                  require("postcss-flexbugs-fixes")
+                ]
+              }
+            },
+            {
+              loader: "sass-loader",
+              options: {
+                precision: 3,
+                sourceMap: ifDevelopment(),
+                outputStyle: ifProduction(
+                  "compressed",
+                  "expanded"
+                ),
+                data: appNamespace
+              }
+            },
+            {
+              loader: "import-glob-loader"
+            }
+          ]
+        }
+      ]
     }
-    return app;
- }
+  };
+  return app;
+};
